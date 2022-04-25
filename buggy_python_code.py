4b912edad1ba85635ea97a013b9efe6369faf0fe
Oliver Bajus <xbajus@fi.muni.cz>
2022-04-25 18:57:04 +0200

    commit

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,19 +1,22 @@
 # contains bunch of buggy examples
 # taken from https://hackernoon.com/10-common-security-gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
-import cPickle
+
 import subprocess
 import base64
 import subprocess
 import flask
 
 # Input injection
+from pywin.Demos.app.helloapp import app
+
+
 def transcode_file(request, filename):
     command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=filename)
     subprocess.call(command, shell=True)  # a bad idea!
 
 
 # Assert statements
-def foo(request, user):
+def floor(request, user):
     assert user.is_admin, 'user does not have access'
     # secure code...
 
